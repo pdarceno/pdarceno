cc30a689d3eb9e674c5032446c46d6a8dc8680fa
prosy <prosy.arceno@mobileoptima.com>
2021-03-10 23:07:22 +0800

    add new form url

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -19,7 +19,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "//formspree.io/contact@pdarceno@gmail.com",
+                url: "//formspree.io/f/maylrezj",
                 method: "POST",
                 data: {
                     name: name,
